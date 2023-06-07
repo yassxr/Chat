916a4881cd24a7bb61e45ef319a357229174717f
yassxr <yassiramine2014@gmail>
2023-06-07 15:52:27 +0100

    Add project files.

---
+++ ChatRoom/Hubs/ChatHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Text.RegularExpressions;
+
+namespace ChatRoom.Hubs
+{
+
+    public class ChatHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Groups.AddToGroupAsync(Context.ConnectionId, Context.User.Identity.Name);
+            return base.OnConnectedAsync();
+        }
+        public async Task SendMessage(string user, string message)
+        {
+            //message send to all users
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public Task SendMessageToGroup(string sender, string receiver, string message)
+        {
+            //message send to receiver only
+            return Clients.Group(receiver).SendAsync("ReceiveMessage", sender, message);
+        }
+    }
+}
